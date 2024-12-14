6646178c4ec347edde36b5b4158d7dcdb83400a4
Christal1803 <chrisy.cjcl@gmail.com>
2024-12-14 23:17:30 +0530

    Tinkering

--- src/app/pages/premade/premade.component.ts
+++ src/app/pages/premade/premade.component.ts
@@ -1,36 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-premade',
   templateUrl: './premade.component.html',
   styleUrls: ['./premade.component.css']
 })
 export class PremadeComponent implements OnInit {
- customOptions: OwlOptions = {
-    loop: true,
-    autoplay: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: true,
-    navText: ['prev', 'next'],
-    navSpeed: 700,
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 5
-      }
-    },
-    nav: true
-  }
+ 
   constructor() { }
 
   ngOnInit(): void {
