63a608c5cbcca57177a6cfc4078655073cd9024c
sasidharan <sasidharan.itprof@gmail.com>
2025-02-14 00:58:36 +0530

    Premade party list fixed

--- src/app/pages/premade/premade.component.ts
+++ src/app/pages/premade/premade.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiAccessService } from 'src/app/servies/api-access.service';
 @Component({
   selector: 'app-premade',
   templateUrl: './premade.component.html',
@@ -6,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PremadeComponent implements OnInit {
 
-  constructor() { }
+  premadeParties: any[] = [];
+
+  constructor(private auth: ApiAccessService) { }
 
   ngOnInit(): void {
+    this.auth.fetchPremadeParties().subscribe((data) => {
+      this.premadeParties = data?.data;
+      if (this.premadeParties.length > 1) {
+        this.premadeParties = this.premadeParties.slice(0, 1);
+      }
+
+    });
+  }
+
+  redirectToPremade(): void {
+    window.location.href = 'https://qa-gaming.ggera.com/client/premade-available';
   }
 
 }
