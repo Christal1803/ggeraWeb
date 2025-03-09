83077c9918e31147deb93a36993559933e9c9a0b
sasidharan <sasidharan.itprof@gmail.com>
2025-03-09 22:40:24 +0530

    Updated premade party streamlink url

--- src/app/pages/premade/premade.component.ts
+++ src/app/pages/premade/premade.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ApiAccessService } from 'src/app/servies/api-access.service';
 @Component({
   selector: 'app-premade',
@@ -8,17 +9,30 @@ import { ApiAccessService } from 'src/app/servies/api-access.service';
 export class PremadeComponent implements OnInit {
 
   premadeParties: any[] = [];
+  fallbackVideoUrl: string = '';
 
-  constructor(private auth: ApiAccessService) { }
+  constructor(private auth: ApiAccessService,
+    private _sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
+    this.fallbackVideoUrl = this.transformVideoUrl('https://www.youtube.com/embed/V08UPqchVgQ');
     this.auth.fetchPremadeParties().subscribe((data) => {
       this.premadeParties = data?.data;
       if (this.premadeParties.length > 1) {
         this.premadeParties = this.premadeParties.slice(0, 1);
       }
+      this.premadeParties.map((party) => party.embedUrl = this.transformVideoUrl(party.party?.videoUrl));
 
     });
+  }
+
+  transformVideoUrl(value: string): any {
+    if (value) {
+      return this._sanitizer.bypassSecurityTrustResourceUrl(value);
+    } else {
+      return '';
+    }
   }
 
   redirectToPremade(): void {
